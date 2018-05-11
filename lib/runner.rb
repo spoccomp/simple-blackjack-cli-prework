329e668255b33c83d34d382539ef641684ec0850
Richard Trapanese <richardtrapanese@gmail.com>
2018-05-11 02:55:14 +0000

    Done.

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,12 @@
 require_relative "blackjack.rb"
 
-runner
+#runner
+def runner
+  # code runner here
+  welcome
+  card_total = initial_round
+  while card_total < 21
+    card_total = hit?(card_total)
+    card_total = display_card_total(card_total)
+  end
+  end_game(card_total)
